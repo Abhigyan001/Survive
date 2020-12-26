bc092af4113dd7fc3b51d539bff68ea96539d97e
ABHIGYAN MAHANTA <abhigyanmahanta001@gmail.com>
2020-12-26 11:14:01 +0530

    Add shooting ability for the player ship

--- src/modules/Entities.js
+++ src/modules/Entities.js
@@ -13,7 +13,10 @@ class Player extends Entity {
   constructor(scene, x, y, key) {
     super(scene, x, y, key, "Player");
     this.setData("speed", 200);
-    this.play("sprPlayer");    
+    this.play("sprPlayer");
+    this.setData("isShooting", false);
+    this.setData("timerShootDelay", 10);
+    this.setData("timerShootTick", this.getData("timerShootDelay") - 1);    
   }
 
   moveUp() {
@@ -37,6 +40,26 @@ class Player extends Entity {
 
     this.x = Phaser.Math.Clamp(this.x, 0, this.scene.game.config.width);
     this.y = Phaser.Math.Clamp(this.y, 0, this.scene.game.config.height);
+
+    if (this.getData("isShooting")) {
+      if (this.getData("timerShootTick") < this.getData("timerShootDelay")) {
+        this.setData("timerShootTick", this.getData("timerShootTick") + 1); 
+      }
+      else {
+        var laser = new PlayerLaser(this.scene, this.x, this.y);
+        this.scene.playerLasers.add(laser);
+      
+        this.scene.sfx.laser.play(); 
+        this.setData("timerShootTick", 0);
+      }
+    }
+  }
+}
+
+class PlayerLaser extends Entity {
+  constructor(scene, x, y) {
+    super(scene, x, y, "sprLaserPlayer");
+    this.body.velocity.y = -200;
   }
 }
 
