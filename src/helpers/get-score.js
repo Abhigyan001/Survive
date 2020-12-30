d2d90664e8262264a6cb432ebcecfd000c036d46
ABHIGYAN MAHANTA <abhigyanmahanta001@gmail.com>
2020-12-30 14:39:38 +0530

    Implement ES6 in modules, add .catch in getScore and fix score table

--- src/helpers/get-score.js
+++ src/helpers/get-score.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 import sortAndSlice from './sort-and-slice';
 
-async function getScore() {
+const getScore = async () => {
   const request = await axios.get('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/njRFjf7c2U2mKScaLf24/scores/', {})
-    .then(res => res.data.result)
-    .then(res => sortAndSlice(res));
+  .then(res => res.data.result)
+  .then(res => sortAndSlice(res)).catch( (error) => {
+    console.log(error.toJSON());
+  });  
   return request;
 }
 
--- src/helpers/send-score.js
+++ src/helpers/send-score.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-async function sendScore(name, score) {
+const sendScore = async (name, score) => {
   const result = await axios.post('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/njRFjf7c2U2mKScaLf24/scores/', { user: name, score })
     .then(res => res.data)
     .catch(err => err.response.data);
